d6d74f94e50a09f7539449e1568d003254410234
mathiaschmid19 <mathias.schmid19@hotmail.com>
2023-03-28 01:05:42 +0000

    Changes to be committed: 	modified:   cookie-consent.php

--- cookie-consent.php
+++ cookie-consent.php
@@ -14,25 +14,3 @@ function egap_insert_tracking_code() {
     }
 }
 add_action('wp_footer', 'egap_insert_tracking_code');
-
-function egap_opt_out_box() {
-    ob_start();
-    ?>
-    <div class="egap-opt-out-box">
-        <p><?php _e('To opt-out of Google Analytics tracking, please check the box below.', 'easy-google-analytics-pro'); ?></p>
-        <input type="checkbox" id="egap-opt-out" />
-        <label for="egap-opt-out"><?php _e('Opt-out of Google Analytics tracking', 'easy-google-analytics-pro'); ?></label>
-    </div>
-    <script>
-        document.getElementById('egap-opt-out').addEventListener('change', function() {
-            if (this.checked) {
-                window['ga-disable-UA-XXXXX-Y'] = true; // Replace 'UA-XXXXX-Y' with your Google Analytics tracking ID
-            } else {
-                window['ga-disable-UA-XXXXX-Y'] = false;
-            }
-        });
-    </script>
-    <?php
-    return ob_get_clean();
-}
-add_shortcode('egap_opt_out_box', 'egap_opt_out_box');
