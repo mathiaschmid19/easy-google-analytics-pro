008d665d7e0832fa440a937503f6b8270a6acf54
mathiaschmid19 <mathias.schmid19@hotmail.com>
2023-03-28 01:20:07 +0000

    Changes to be committed: 	modified:   tracking-code.php

--- tracking-code.php
+++ tracking-code.php
@@ -1,12 +1,11 @@
 <?php
 
-function egap_output_tracking_code() {
+// Add Google Analytics tracking code to the website.
+function egap_insert_tracking_code() {
     if (!isset($_COOKIE['egap_cookie_consent']) || $_COOKIE['egap_cookie_consent'] !== 'accepted') {
         return;
     }
 
-// Add Google Analytics tracking code to the website.
-function egap_insert_tracking_code() {
     $egap_settings = get_option('egap_settings');
     $tracking_id = isset($egap_settings['tracking_id']) ? $egap_settings['tracking_id'] : '';
 
@@ -14,7 +13,6 @@ function egap_insert_tracking_code() {
     if ($tracking_id) {
         // Your tracking code output here.
     }
-}
 }
 add_action('wp_footer', 'egap_insert_tracking_code');
 
