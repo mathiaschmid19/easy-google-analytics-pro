337c4c0f994cb6b6811cdd631d59cc8d57c7b719
mathiaschmid19 <mathias.schmid19@hotmail.com>
2023-03-28 00:29:12 +0000

    Changes to be committed: 	modified:   assets/css/cookie-consent-style.css 	modified:   cookie-consent.php

--- cookie-consent.php
+++ cookie-consent.php
@@ -2,27 +2,26 @@
 
 function egap_cookie_consent_banner() {
     $egap_settings = get_option('egap_settings');
-    $show_cookie_consent = isset($egap_settings['egap_show_cookie_consent']) ? $egap_settings['egap_show_cookie_consent'] : '';
+    $cookie_consent_enabled = isset($egap_settings['egap_enable_cookie_consent']) ? $egap_settings['egap_enable_cookie_consent'] : '';
 
-    // Only display the cookie consent banner if the option is enabled.
-    if ($show_cookie_consent) {
-        if (!isset($_COOKIE['egap_cookie_consent']) || $_COOKIE['egap_cookie_consent'] !== 'accepted') {
-            ?>
-            <div id="egap-cookie-consent" class="egap-cookie-consent">
-                <p><?php _e('We use cookies to analyze our website traffic and improve your experience. By using our site, you agree to our use of cookies.', 'easy-google-analytics-pro'); ?></p>
-                <button id="egap-cookie-consent-accept"><?php _e('Accept', 'easy-google-analytics-pro'); ?></button>
-            </div>
-            <?php
-        }
+    if ($cookie_consent_enabled && (!isset($_COOKIE['egap_cookie_consent']) || $_COOKIE['egap_cookie_consent'] !== 'accepted')) {
+        ?>
+        <div id="egap-cookie-consent" class="egap-cookie-consent">
+            <p><?php _e('We use cookies to analyze our website traffic and improve your experience. By using our site, you agree to our use of cookies.', 'easy-google-analytics-pro'); ?></p>
+            <button id="egap-cookie-consent-accept"><?php _e('Accept', 'easy-google-analytics-pro'); ?></button>
+        </div>
+        <?php
     }
 }
 add_action('wp_footer', 'egap_cookie_consent_banner');
 
 function egap_enqueue_cookie_consent_assets() {
-    if (!isset($_COOKIE['egap_cookie_consent']) || $_COOKIE['egap_cookie_consent'] !== 'accepted') {
+    $egap_settings = get_option('egap_settings');
+    $cookie_consent_enabled = isset($egap_settings['egap_enable_cookie_consent']) ? $egap_settings['egap_enable_cookie_consent'] : '';
+
+    if ($cookie_consent_enabled && (!isset($_COOKIE['egap_cookie_consent']) || $_COOKIE['egap_cookie_consent'] !== 'accepted')) {
         wp_enqueue_style('egap-cookie-consent-style', plugin_dir_url(__FILE__) . 'assets/css/cookie-consent-style.css', array(), '1.0.0');
         wp_enqueue_script('egap-cookie-consent-script', plugin_dir_url(__FILE__) . 'assets/js/cookie-consent-script.js', array(), '1.0.0', true);
     }
 }
 add_action('wp_enqueue_scripts', 'egap_enqueue_cookie_consent_assets');
-
