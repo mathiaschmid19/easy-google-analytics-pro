bda64f6b71f8a8d8050a6b1f28cb8a226d304cd6
mathiaschmid19 <mathias.schmid19@hotmail.com>
2023-03-28 01:12:43 +0000

    Changes to be committed: 	modified:   cookie-consent.php

--- cookie-consent.php
+++ cookie-consent.php
@@ -1,16 +1,28 @@
 <?php
 
-function egap_insert_tracking_code() {
-    if (!isset($_COOKIE['egap_cookie_consent']) || $_COOKIE['egap_cookie_consent'] !== 'accepted') {
-        return;
-    }
+function egap_cookie_consent_banner() {
+    $options = get_option('egap_settings');
+    $show_cookie_consent_banner = isset($options['egap_show_cookie_consent_banner']) ? $options['egap_show_cookie_consent_banner'] : '';
 
-    $egap_settings = get_option('egap_settings');
-    $tracking_id = isset($egap_settings['tracking_id']) ? $egap_settings['tracking_id'] : '';
-
-    // Check if the tracking ID is set.
-    if ($tracking_id) {
-        // Your tracking code output here.
+    if ($show_cookie_consent_banner && (!isset($_COOKIE['egap_cookie_consent']) || $_COOKIE['egap_cookie_consent'] !== 'accepted')) {
+        ?>
+        <div id="egap-cookie-consent" class="egap-cookie-consent">
+            <p><?php _e('We use cookies to analyze our website traffic and improve your experience. By using our site, you agree to our use of cookies.', 'easy-google-analytics-pro'); ?></p>
+            <button id="egap-cookie-consent-accept"><?php _e('Accept', 'easy-google-analytics-pro'); ?></button>
+        </div>
+        <?php
     }
 }
-add_action('wp_footer', 'egap_insert_tracking_code');
+add_action('wp_footer', 'egap_cookie_consent_banner');
+
+function egap_enqueue_cookie_consent_assets() {
+    $options = get_option('egap_settings');
+    $show_cookie_consent_banner = isset($options['egap_show_cookie_consent_banner']) ? $options['egap_show_cookie_consent_banner'] : '';
+
+    if ($show_cookie_consent_banner && (!isset($_COOKIE['egap_cookie_consent']) || $_COOKIE['egap_cookie_consent'] !== 'accepted')) {
+        wp_enqueue_style('egap-cookie-consent-style', plugin_dir_url(__FILE__) . 'assets/css/cookie-consent-style.css', array(), '1.0.0');
+        wp_enqueue_script('egap-cookie-consent-script', plugin_dir_url(__FILE__) . 'assets/js/cookie-consent-script.js', array(), '1.0.0', true);
+    }
+}
+add_action('wp_enqueue_scripts', 'egap_enqueue_cookie_consent_assets');
+
